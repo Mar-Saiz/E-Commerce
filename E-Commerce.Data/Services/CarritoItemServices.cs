de4c04793b09a38601995ece804d1dd351fbb078
Maria Abreu <mariaabreums05@gmail.com>
2025-10-24 15:16:49 -0400

    Added all the necesarry methos in coupons and cartitems

--- E-Commerce.Data/Services/CarritoItemServices.cs
+++ E-Commerce.Data/Services/CarritoItemServices.cs
@@ -10,10 +10,72 @@ namespace E_Commerce.Data.Services
     {
         public readonly ICarritoItemRepository _carritoItemServices;
         public readonly IMapper _mapper;
+        public readonly IProductoServices _productoServices;
         public CarritoItemServices(ICarritoItemRepository carrito, IMapper mapper) : base(mapper, carrito)
         {
             _mapper = mapper;
             _carritoItemServices = carrito;
         }
+
+        public async Task<CarritoItemDto> CreateCartAsync (CarritoItemDto AppUserId)
+        {
+            var entitie = _mapper.Map<CarritoItem>(AppUserId);
+
+           return  _mapper.Map<CarritoItemDto>( await _carritoItemServices.SaveEntityAsync(entitie));
+
+        }
+
+        public async Task<CarritoItemDto> AddItemToCarritoAsync(int ProductID,int cantidad, CarritoItemDto Carrito)
+        {
+         
+            CarritoItem NewCarrito = new CarritoItem
+            {
+                ProductoId = ProductID,
+                Cantidad = cantidad,
+                UserId = Carrito.UserId
+            };
+
+            var result = _mapper.Map<CarritoItemDto>( await _carritoItemServices.UpdateEntityAsync(Carrito.Id, NewCarrito));
+
+            return result;
+        }
+
+        public async Task<CarritoItemDto> RemoveItemToCarritoAsync(int ProductID, int cantidad, CarritoItemDto Carrito)
+        {
+
+            if (Carrito.ProductoId != ProductID)
+            {
+                Console.WriteLine("El producto no coincide con el producto del carrito.");
+                return null;
+            }
+
+            CarritoItem NewCarrito = new CarritoItem
+            {
+               ProductoId = 0,
+               Cantidad = Carrito.Cantidad > cantidad ? Carrito.Cantidad - cantidad : 0,
+               UserId = Carrito.UserId
+            };
+
+             var result = _mapper.Map<CarritoItemDto>(await _carritoItemServices.UpdateEntityAsync(Carrito.Id, NewCarrito));
+
+            return result;
+        }
+
+        public async Task<decimal> CalculateTotal(CarritoItemDto Carrito)
+        { decimal total = 0;
+
+            if (Carrito.Subtotal == 0)
+            {
+               var producto = await _productoServices.GetDtoById(Carrito.ProductoId);
+
+              return  total = producto.Precio * Carrito.Cantidad * 0.18m;
+            }
+
+            decimal itbis = 0.18m;
+
+            return total = Carrito.Subtotal * itbis;
+
+        }
+
     }
 }
--- E-Commerce.Data/Services/CuponServices.cs
+++ E-Commerce.Data/Services/CuponServices.cs
@@ -15,6 +15,34 @@ namespace E_Commerce.Data.Services
             _mapper = mapper;
             _categoriaRepository = cuponRepository;
         }
-    
+
+        // validar cupon.
+
+        public async Task<CuponDto> ValidarCuponAsync(CuponDto cuponDto)
+        {
+            var cupon = await _categoriaRepository.GetEntityByIdAsync(cuponDto.Id);
+            if (cupon == null || cupon.FechaExpiracion < DateTime.Now)
+            {
+                return null; // Cupón inválido o expirado
+            }
+            return _mapper.Map<CuponDto>(cupon);
+        }
+
+
+        // aplicar cupon al carrito
+
+        public async Task<decimal> AplicarCuponAlCarritoAsync(CuponDto cuponDto, decimal totalCarrito)
+        {
+            var cupon = await ValidarCuponAsync(cuponDto);
+            if (cupon == null)
+            {
+                return 0;
+            }
+            decimal descuento = (totalCarrito * cupon.Descuento);
+
+            return totalCarrito - descuento;
+        }
+
+
     }
 }
