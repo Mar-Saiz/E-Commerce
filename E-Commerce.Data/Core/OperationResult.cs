8745f7da5800110bec63e7a8ed5e854874bb29b4
Jam1ll <jamilguzman202@gmail.com>
2025-10-24 12:55:12 -0400

    unit test Core/OperationResult.cs class done

---
+++ E-Commerce.Data/Core/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Core
+{
+    public class OperationResult<TData>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public TData? Result { get; set; }
+    }
+}
