42ac3291a6a2d938e95a28550bf076a6b64b1aa5
Maria Abreu <mariaabreums05@gmail.com>
2025-10-28 10:51:42 -0400

    Defined Pagos and PagosDto classes

---
+++ E-Commerce.Data/Entities/Pagos.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Data.Interfaces.Base;
+
+namespace E_Commerce.Data.Entities
+{
+    public class Pagos : BaseEntityDto<int>
+    {
+        public override int Id { get; set; }
+        public  string UserId { get; set; }
+        public  string MetodoPago { get; set; }
+        public  decimal Monto { get; set; }
+        public  int PedidoId { get; set; }
+
+
+    }
+}
