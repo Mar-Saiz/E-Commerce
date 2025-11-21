cb6c30bef4ada1ddaf5d54afb2e50f1f9896590b
Mariana <marianaemiliasanchez24@gmail.com>
2025-11-21 09:16:25 -0400

    Vista Entidad Promociones, correcciones en producto y categoria

---
+++ E-Commerce.Data/ViewModels/PromocionViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace E_Commerce.Data.ViewModels
+{
+    public class PromocionViewModel: IBaseViewModel<int>
+    {
+        public override int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
