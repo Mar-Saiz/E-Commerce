8e95d192ccd860f869d176f8487e069a08dd0ba8
Mariana <marianaemiliasanchez24@gmail.com>
2025-11-20 22:07:11 -0400

    Vistas gestion de cupones

--- E-Commerce.Data/ViewModels/SaveCuponViewModel.cs
+++ E-Commerce.Data/ViewModels/SaveCuponViewModel.cs
@@ -1,8 +1,10 @@
-﻿namespace E_Commerce.Data.DTOs.EntititesDto
+﻿
+
+namespace E_Commerce.Data.ViewModels
 {
-    public class CuponDto : BaseEntityDto<int>
+    public class SaveCuponViewModel
     {
-        public override int Id { get; set; }
+        public int Id { get; set; }
         public string Codigo { get; set; }
         public decimal Descuento { get; set; }
         public DateTime? FechaExpiracion { get; set; }
